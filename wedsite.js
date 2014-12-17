06f24c26c6af7d24d7ebaab91af86539ab76ce7f
Seth Root <sethElijah@gmail.com>
2014-12-16 23:46:10 -0600

    fixes google maps issue

--- wedsite.js
+++ wedsite.js
@@ -1,17 +1,11 @@
 function drawMap(canvasID, config) {
-    var mapOptions = {
+    map = new google.maps.Map(document.getElementById(canvasID),{
         center: new google.maps.LatLng(config.center[0], config.center[1]),
         zoom: 16,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    var map = new google.maps.Map(
-            document.getElementById(canvasID),
-            mapOptions);
+        mapTypeId: google.maps.MapTypeId.ROADMAP});
     
-    var position;
     for (var i=0; i<config.positions.length; i++) {
-        position = config.positions[i];
+        var position = config.positions[i];
         new StyledMarker({
             styleIcon: new StyledIcon(StyledIconTypes.BUBBLE, {color: "EEEEEE", text: position.label}),
             position: new google.maps.LatLng(position.latlng[0], position.latlng[1]),
@@ -20,64 +14,23 @@ function drawMap(canvasID, config) {
 };
 
 $(document).ready(function(){
-            $( "#tabs" ).tabs();
-            var options = {
-                $AutoPlay: true,
-                $AutoPlayInterval: 4000,
-                $SlideDuration: 500,
-                $DragOrientation: 3,
-                $UISearchMode: 0,
-                $FillMode: 1,
+    var copernicus = {
+        label: 'Ceremony',
+        latlng: [41.968071, -87.758610]
+    };
 
-                $ThumbnailNavigatorOptions: {
-                    $Class: $JssorThumbnailNavigator$, 
-                    $ChanceToShow: 2,
-
-                    $Loop: 2,
-                    $SpacingX: 3,
-                    $SpacingY: 3,
-                    $DisplayPieces: 6,
-                    $ParkingPosition: 204,
-
-                    $ArrowNavigatorOptions: {
-                        $Class: $JssorArrowNavigator$,
-                        $ChanceToShow: 2,
-                        $AutoCenter: 2,
-                        $Steps: 6
-                    }
-                }
-            };
-
-            var jssor_slider1 = new $JssorSlider$("slider1_container", options);
-
-            //responsive code begin
-            //you can remove responsive code if you don't want the slider scales while window resizes
-            function ScaleSlider() {
-                var parentWidth = jssor_slider1.$Elmt.parentNode.clientWidth;
-                if (parentWidth)
-                    jssor_slider1.$ScaleWidth(Math.min(parentWidth, 720));
-                else
-                    window.setTimeout(ScaleSlider, 30);
-            }
-            ScaleSlider();
-
-            $(window).bind("load", ScaleSlider);
-            $(window).bind("resize", ScaleSlider);
-            $(window).bind("orientationchange", ScaleSlider);
-            //responsive code end
-            
-            var copernicus = {
-                label: 'Ceremony',
-                latlng: [41.968071, -87.758610]
-            };
-
-            var jeffersonCTA = {
-                label: 'Jefferson Park CTA Station',
-                latlng: [41.969880, -87.761852]
-            };
-
+    var jeffersonCTA = {
+        label: 'Jefferson Park CTA Station',
+        latlng: [41.969880, -87.761852]
+    };
+    
+    var map;
+    $('a[href="#location"]').on("shown.bs.tab", function (e) {
+        if( map == undefined) {
             drawMap('map', {
                 center: [41.968811, -87.759883],
                 positions: [copernicus, jeffersonCTA]
             });
-        });
+        }
+    });
+});
